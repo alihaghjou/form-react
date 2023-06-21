38ad2cd145811a6e5de6c583d489b59a452890c2
a-h <alihaghjou30@yahoo.com>
2023-06-21 21:30:19 +0330

    react hook form added

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,64 @@
-import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const zod = z.object({
   email: z.string().email({ message: "Enter a valid Email" }),
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
+  firstName: z
+    .string()
+    .min(2, { message: "Must contain at least 2 character" }),
+  lastName: z.string().min(2, { message: "Must contain at least 2 character" }),
   password: z
     .string()
-    .min(8)
+    .min(8, { message: "Must contain at least 8 character" })
     .regex(/[a-z]/, { message: "Must Contain a Lowercase" })
     .regex(/[A-Z]/, { message: "Must Contain an UpperCase" })
     .regex(/[0-9]/, { message: "Must Contain a Number" }),
 });
 
-function App() {
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [password, setPassword] = useState("");
+type FormData = z.infer<typeof zod>;
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    zod.parse({ email, firstName, lastName, password });
-    console.log(email, firstName, lastName, password);
-  }
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(zod),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
 
   return (
     <form
-      onSubmit={(e) => handleSubmit(e)}
+      onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col p-8 gap-6 w-full"
     >
       <div>
         <label htmlFor="Email">Email Input:</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          id="Email"
-          type="text"
-        />
+        <input {...register("email")} id="Email" type="text" />
+        <p className="errorMessage">{errors.email?.message}</p>
       </div>
       <div>
         <label htmlFor="First Name">First Input:</label>
-        <input
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-          id="First Name"
-          type="text"
-        />
+        <input {...register("firstName")} id="First Name" type="text" />
+        <p className="errorMessage">{errors.firstName?.message}</p>
       </div>
       <div>
         <label htmlFor="Last Name">Last Input:</label>
-        <input
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-          id="Last Name"
-          type="text"
-        />
+        <input {...register("lastName")} id="Last Name" type="text" />
+        <p className="errorMessage">{errors.lastName?.message}</p>
       </div>
       <div>
         <label htmlFor="password">Password:</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          id="password"
-          type="password"
-        />
+        <input {...register("password")} id="password" type="password" />
+        <p className="errorMessage">{errors.password?.message}</p>
       </div>
       <div className="flex items-center justify-center">
         <button
-          className="py-4 px-6 border-2 rounded border-slate-200 hover:text-slate-800 hover:bg-slate-200"
+          className="py-4 text-xl px-6 border-2 rounded border-slate-200 hover:text-slate-800 hover:bg-slate-200"
           type="submit"
         >
           Submit
